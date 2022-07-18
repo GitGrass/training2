76ad9e43a2b0ad43c49b50d516fd64bba293de0b
youseethree <y.sato@sens.sys.es.osaka-u.ac.jp>
2022-07-18 15:07:35 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+## Requirement
+- python 3.7 (多分python3.5以上だとなんでもOK)
+- Flask (conda install Flask、サーバ立てる用)
+- numpy
+- tensorflow
+- keras
+
+## Initial setup
+1. Requirementの環境を整える。
+2. root directoryでpython server.pyを実行する。
+3. http://localhost:xxxx/ (xxxx : 実行時に指定されるポート)にアクセスする。
+4. ファイルを選択ボックスで画像ファイルを選択肢てEstimateボタンを押す
+5. その画像のクラスが表示される
---
+++ webapp/views.py
@@ -0,0 +1,25 @@
+import os
+from flask import render_template, request
+from webapp import app
+
+# 深層学習結果をロードする
+from webapp.image2text import Image2Text
+
+UPLOAD_FOLDER = './uploads'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+estimator = Image2Text()
+
+@app.route('/', methods=['GET'])
+def show_main_page():
+    return render_template("viewer.html")
+
+@app.route('/estimate', methods=['POST'])
+def estimate_category_from_image():
+    save_filename = os.path.join(app.config['UPLOAD_FOLDER'], f'upload.png')
+    
+    if 'image-file' in request.files:
+        request.files['image-file'].save(save_filename)
+
+    name = estimator.predict(save_filename)
+    return name
